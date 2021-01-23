3b140f7740f024391955953122c26c7b4dde94ae
petruki <31597636+petruki@users.noreply.github.com>
2021-01-22 18:15:22 -0800

    Fixed code smell on ExecutionLogger

--- src/lib/executionLogger.js
+++ src/lib/executionLogger.js
@@ -5,9 +5,11 @@ var logger = new Array();
 class ExecutionLogger {
 
     static add(key, reasponse) {
-        const replace = this.getByKey(key);
-        if (replace.length)
-            logger.splice(replace, 1);
+        let keyIndex = undefined;
+        logger.map((value, index) => value.key === key ? keyIndex = index : undefined);
+
+        if (keyIndex != undefined)
+            logger.splice(keyIndex, 1);
         logger.push({ key, reasponse });
     }
 
