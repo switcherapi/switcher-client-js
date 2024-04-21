65e81b0e06a41d84ce3618eaca55dd6e9042f2ae
petruki <31597636+petruki@users.noreply.github.com>
2024-04-20 19:38:40 -0700

    Refactored throttle token renew test acceptance

--- test/switcher-functional.test.js
+++ test/switcher-functional.test.js
@@ -126,28 +126,39 @@ describe('Integrated test - Switcher:', function () {
       // first API call
       given(fetchStub, 0, { json: () => generateAuth('[auth_token]', 1), status: 200 });
       given(fetchStub, 1, { json: () => generateResult(true), status: 200 }); // before token expires
-      given(fetchStub, 2, { json: () => generateResult(false), status: 200 }); // after token expires
-
+      
       // test
       Switcher.buildContext(contextSettings);
-
+      
       let switcher = Switcher.factory();
       switcher.throttle(500);
-
+      
       const spyPrepare = spy(switcher, 'prepare');
-
+      
       // first API call - stores result in cache
       let result = await switcher.isItOn('FLAG_3');
       assert.isTrue(result);
       assert.equal(spyPrepare.callCount, 1);
-
+      
       // first async API call
       result = await switcher.isItOn('FLAG_3');
       assert.isTrue(result);
       assert.equal(spyPrepare.callCount, 1);
-
+      
       // Next call should call the API again - token has expired
       await new Promise(resolve => setTimeout(resolve, 2000));
+      
+      // given
+      given(fetchStub, 3, { json: () => generateAuth('[auth_token]', 5), status: 200 });
+      given(fetchStub, 4, { json: () => generateResult(false), status: 200 }); // after token expires
+
+      // test - stores result in cache after token renewal
+      await new Promise(resolve => setTimeout(resolve, 500));
+      result = await switcher.isItOn('FLAG_3');
+      assert.isTrue(result);
+      assert.equal(spyPrepare.callCount, 2);
+
+      await new Promise(resolve => setTimeout(resolve, 50));
       result = await switcher.isItOn('FLAG_3');
       assert.isFalse(result);
       assert.equal(spyPrepare.callCount, 2);
